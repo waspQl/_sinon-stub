5d54bee890267727e20d810259c07af2b33c34ac
sakutaro <u390862@gmail.com>
2020-02-24 15:08:25 +0900

    update: コメント追加

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,39 @@ import assert from 'assert'
 
 const stub  = sinon.stub()
 
-stub.returns(7)
-assert.equal(stub(), 7)
+const randomNum = (): number => {
+  return Math.floor(Math.random() * 10000)
+}
 
-stub.withArgs(42).returns(1)
-assert.equal(stub(42), 1)
+// 未設定時はundefinedを返す
+assert.equal(stub(randomNum()), undefined)
 
-stub.withArgs(1).throws('Error')
+// default設定
+// stub() か 未設定の引数が設定された時にdefaultResNumを返す
+const defaultResNum = randomNum()
+stub.returns(defaultResNum)
+assert.equal(
+  stub(),
+  defaultResNum
+)
+assert.equal(
+  stub(randomNum()),
+  defaultResNum
+)
+
+// 引数受取時の動作
+const argNum = randomNum()
+const argErrorNum = randomNum()
+const argResNum = randomNum()
+// 1. 正常系
+stub.withArgs(argNum).returns(argResNum)
+assert.equal(
+  stub(argNum),
+  argResNum
+)
+// 2. 異常系
+stub.withArgs(argErrorNum).throws('Error')
 assert.throws(
-  stub.bind(null, 1),
+  stub.bind(null, argErrorNum),
   Error
-);
+)
